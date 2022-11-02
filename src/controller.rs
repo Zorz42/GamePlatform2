009d3d1d4f384690d22fdea7cd240bbf80491d34
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2022-11-02 19:41:40 +0100

    added controller buttons

--- src/controller.rs
+++ src/controller.rs
@@ -3,6 +3,22 @@ pub struct ControllerManager {
     controller_id: u32
 }
 
+pub enum ControllerButton {
+    Cross = 1,
+    Circle = 2,
+    Triangle = 3,
+    Square = 0,
+    Right1 = 5,
+    Right2 = 7,
+    Left1 = 4,
+    Left2 = 6,
+    Share = 8,
+    Options = 9,
+    RightAxis = 11,
+    LeftAxis = 10,
+    Pad = 13,
+}
+
 impl ControllerManager {
     pub fn new() -> Self {
         ControllerManager{ controller_id: NO_CONTROLLER }
@@ -21,5 +37,9 @@ impl ControllerManager {
 
     pub fn is_controller_connected(&self) -> bool {
         self.controller_id != NO_CONTROLLER
+    }
+
+    pub fn is_button_pressed(&self, button: ControllerButton) -> bool {
+        sfml::window::joystick::is_button_pressed(self.controller_id, button as u32)
     }
 }
