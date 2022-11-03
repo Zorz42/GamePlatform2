04e6361dabeeefb0e7c7708f965423da62395aad
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2022-11-03 14:56:17 +0100

    added an attempt of doing anything in rust

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,30 @@ mod controller;
 mod graphics;
 
 use sfml;
-use sfml::graphics::{RenderTarget, Transformable};
+use sfml::graphics::{Drawable, RenderTarget, Shape, Transformable};
+
+
+struct Tile<'a> {
+    rect: sfml::graphics::RectangleShape<'a>,
+
+}
+
+impl Tile<'_> {
+    pub fn new() -> Self {
+        let mut result = Tile{
+            rect: sfml::graphics::RectangleShape::new(),
+        };
+        result.rect.set_size(sfml::system::Vector2f::new(500.0, 500.0));
+        result.rect.set_fill_color(sfml::graphics::Color::BLUE);
+        result
+    }
+
+    pub fn draw(&mut self, offset: f32, graphics_manager: &mut graphics::GraphicsManager) {
+        self.rect.set_position(sfml::system::Vector2f::new(offset, 500.0));
+        graphics_manager.window.draw(&self.rect);
+    }
+}
+
 
 
 fn main() {
@@ -11,9 +34,12 @@ fn main() {
     let mut controller_manager = controller::ControllerManager::new();
     controller_manager.update(&mut graphics_manager);
 
-    //let mut text = sfml::graphics::Text::new("Controller is connected", &graphics_manager.font, 64);
-    //text.set_position(sfml::system::Vector2f::new(50.0, 50.0));
+    let mut text = graphics_manager.create_text("Controller is connected");
+    let mut text_sprite = sfml::graphics::Sprite::new();
+    text_sprite.set_texture(&text.texture(), true);
+    text_sprite.set_position(sfml::system::Vector2f::new(50.0, 50.0));
 
+    let mut tiles = vec![Tile::new(), Tile::new(), Tile::new()];
 
     while graphics_manager.window.is_open() {
         while let Some(e) = graphics_manager.window.poll_event() {
@@ -27,7 +53,13 @@ fn main() {
         controller_manager.update(&mut graphics_manager);
 
         graphics_manager.window.clear(sfml::graphics::Color::BLACK);
-        //graphics_manager.window.draw(&text);
+        graphics_manager.window.draw(&text_sprite);
+
+        let mut curr_x = 0.0;
+        for i in 0..tiles.len() {
+            tiles[i].draw(curr_x, &mut graphics_manager);
+            curr_x += 50.0;
+        }
 
         graphics_manager.window.display()
     }
